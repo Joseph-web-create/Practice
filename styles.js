976e05a20565228b32cd5021740b22862045f49a
Joseph <aacejovi@gmail.com>
2024-06-07 08:32:28 +0100

    Practicing git

---
+++ styles.js
@@ -0,0 +1,15 @@
+let greeting = "Hey what's good!?";
+let number = 1;
+
+for (let i = 1; i <= 10; i++){
+    console.log(greeting + number)
+}
+
+if (greeting === "Hey what's good!?" && number <= 1){
+    console.log("you can enter the club!")
+}
+else{
+    console.log("Home ways lol!")
+}
+
+alert("Hello");
